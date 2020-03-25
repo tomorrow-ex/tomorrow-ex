d36444882518b7dd5ae0282c4bdd4832481564de
tomorrow-ex <tomoyo.kikuchi@litalico.co.jp>
2020-03-25 16:43:11 +0900

    最初のコミット

---
+++ Unity_Action/Assets/CameraMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    public GameObject Player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float posx = Player.transform.position.x;
+        float posy = Player.transform.position.y;
+        float posz = Player.transform.position.z;
+
+        transform.position = new Vector3(posx, posy + 10, posz - 10);
+	}
+}
